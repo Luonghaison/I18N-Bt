ab16f57d3dd72659cda6578e0a7bd29c0bd74d5d
Nguyencongnguyen1812 <ngannt33@wru.vn>
2023-06-03 19:11:01 +0700

    kk

--- src/main/java/ra/config/AppConfig.java
+++ src/main/java/ra/config/AppConfig.java
@@ -3,9 +3,11 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.format.FormatterRegistry;
@@ -119,6 +121,16 @@ public class AppConfig implements WebMvcConfigurer, ApplicationContextAware{
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         return properties;
     }
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+
+
     @Bean
     ICustomerService customerService(){
         return new CustomerServiceImpl();
